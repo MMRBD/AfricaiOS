703391aa3342c9f989e146eed8a7d1e93c4f12d2
Moklesur Rahman <mmrrahman508@gmail.com>
2021-12-24 23:42:06 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Moklesur Rahman on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
