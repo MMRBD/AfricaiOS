c3755abb89425c11641bec4a24a0fabfac253e0b
Moklesur Rahman <mmrrahman508@gmail.com>
2021-12-26 00:47:55 +0600

    view file added

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,31 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Moklesur Rahman on 25/12/21.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+//        MARK: PROPERTISE
+        let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+        
+//        MARK: BODY
+        TabView{
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                .scaledToFill()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+    }
+}
