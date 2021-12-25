e779b33e3c69ea7da53d50ade04b0764ca91baee
Moklesur Rahman <mmrrahman508@gmail.com>
2021-12-26 00:48:15 +0600

    models added

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Moklesur Rahman on 25/12/21.
+//
+
+import Foundation
+
+struct CoverImage:Codable, Identifiable {
+    let id:Int
+    let name:String
+}
+
